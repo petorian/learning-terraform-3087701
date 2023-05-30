c34cc4b9b3bcaeea3efb48f654f2f59f30f98833
petorian <petorian@gmail.com>
2023-05-30 11:41:28 +0200

    .

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.0.2"
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
