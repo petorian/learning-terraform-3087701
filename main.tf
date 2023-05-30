3aed361fbad6181b746f92abcde640ac9fb6f74d
petorian <petorian@gmail.com>
2023-05-30 11:54:06 +0200

    f

--- main.tf
+++ main.tf
@@ -17,11 +17,12 @@ data "aws_ami" "app_ami" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "4.0.2"
-  
+
   name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
